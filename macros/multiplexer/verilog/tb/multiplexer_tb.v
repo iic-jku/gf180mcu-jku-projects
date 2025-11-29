01e262d46921fd2a84ddca9c280d55de2e7daf98
Simon Dorrer <simon.dorrer@jku.at>
2025-11-29 17:42:08 +0100

    added simple testbench for multiplexer

--- macros/multiplexer/verilog/tb/multiplexer_tb.v
+++ macros/multiplexer/verilog/tb/multiplexer_tb.v
@@ -17,11 +17,36 @@
 module multiplexer_tb;
 
   // inputs
-  reg clk       = 1'b0; // 25 MHz
-  wire uio_in	= 1'b1;
+  reg  clk       			= 1'b0; // 25 MHz
+  reg  [2:0] design_sel_in  = 3'b000;
+  wire [3:0] uio_in			= 4'b1111;
+  wire [6:0] ui_in			= 7'b1111111;
+  wire [7:0]dac_bits_in		= 8'b11111111;
+  wire spi_miso_in			= 1'b1;
+  wire spi_sclk_in			= 1'b1;
+  wire done_in				= 1'b1;
+  wire [6:0] seg_bits_in	= 7'b1111111;
+  wire dp_on_in				= 1'b1;
+  wire signal_bit_in 		= 1'b1;
+  wire [7:0] data_in		= 8'b11111111;
+  wire car_red_light_in		= 1'b1;
+  wire car_yellow_light_in	= 1'b1;
+  wire car_green_light_in	= 1'b1;
+  wire ped_red_light_in		= 1'b1;
+  wire ped_green_light_in	= 1'b1;
+  wire DIN_in				= 1'b1;
+  wire CS_in				= 1'b1;
+  wire SCLK_in				= 1'b1;
+  wire pushed_left_in		= 1'b1;
+  wire pushed_right_in		= 1'b1;
+  wire sound_in				= 1'b1;
+  wire buzzer_in 			= 1'b1;
+  wire vga_horizSync_in		= 1'b1;
+  wire vga_vertSync_in		= 1'b1;
+  wire black_white_in		= 1'b1;
 
   // outputs
-  // ToDo
+  wire [10:0] mux_out;
 
   // dut
   multiplexer dut (
@@ -64,14 +89,29 @@ module multiplexer_tb;
     $dumpfile("multiplexer_tb.vcd");
     $dumpvars(0, multiplexer_tb);
     $dumpvars(0, multiplexer_tb.dut);
-
-
-    // reset deactivated
-    #200 rst_n = 1'b1;
-
-    // ToDo
-
-    $finish;
+	
+	#200
+	design_sel_in  = 3'b001;
+	
+	#200
+	design_sel_in  = 3'b010;
+	
+	#200
+	design_sel_in  = 3'b011;
+	
+	#200
+	design_sel_in  = 3'b100;
+	
+	#200
+	design_sel_in  = 3'b101;
+	
+	#200
+	design_sel_in  = 3'b110;
+	
+	#200
+	design_sel_in  = 3'b111;
+	
+    #200 $finish;
     /* verilator lint_on STMTDLY */
   end
 endmodule
